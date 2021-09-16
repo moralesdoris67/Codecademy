8b9dcda65361fd388d2b03f45533a6f96ebb143a
Doris Morales <moralesdoris67@gmail.com>
2021-09-16 08:04:07 -0400

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,10 @@
+#Doris Morales
+#I love to travel
+
+print("DDDD       MM         MMM ")
+print("D    D     M  M      M  M ")
+print("D     D    M    M  M    M ")
+print("D      D   M     M      M ")
+print("D     D    M            M ")
+print("D    D     M            M ")
+print("DDDD       M            M ")
